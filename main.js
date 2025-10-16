dbe368624ae130d9601f951a9751fd63d217a1d7
BanLowi <lowi40k@gmail.com>
2025-10-16 16:00:16 +0200

    input and submit button appear after 30 seconds

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const numberThreeEl = document.getElementById("numberThree");
 const numberFourEl = document.getElementById("numberFour");
 const numberFiveEl = document.getElementById("numberFive");
 
+// Variabili elementi input
+const buttonEl = document.querySelector("button.d-none");
+
+// Variabile contatore
+const counter = 0;
+
 // Inserisco un numero random da 1 a 100 in ogni elemento
 numberOneEl.innerHTML = getRndNumber(1, 100);
 numberTwoEl.innerHTML = getRndNumber(1, 100);
@@ -17,9 +23,18 @@ numberFiveEl.innerHTML = getRndNumber(1, 100);
 
 // Generatore numeri random
 function getRndNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-setTimeout( () => {
-    numbers.className = "d-none"
-}, 30000)
+// Alla fine del timer switcho i numeri random con degli input
+setTimeout(() => {
+
+    numberOneEl.innerHTML = `<input type="number" placeholder="Che numero era?"></input>`
+    numberTwoEl.innerHTML = `<input type="number" placeholder="Che numero era?"></input>`
+    numberThreeEl.innerHTML = `<input type="number" placeholder="Che numero era?"></input>`
+    numberFourEl.innerHTML = `<input type="number" placeholder="Che numero era?"></input>`
+    numberFiveEl.innerHTML = `<input type="number" placeholder="Che numero era?"></input>`
+
+    buttonEl.classList.remove("d-none")
+
+}, 3000)
