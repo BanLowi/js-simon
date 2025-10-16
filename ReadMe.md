82599967bfac4d63094b237a7cfbb08c6fe48ecc
BanLowi <lowi40k@gmail.com>
2025-10-16 15:28:24 +0200

    add random number generator for 30 seconds

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+## -Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+- const counterValidNumber = 0
+
+- Stampo in pagina 5 random_number per 30 secondi.
+
+- Cambio il contenuto in user_input
+    -SE user_input = random_number
+        counterValidNumber++
--- main.js
+++ main.js
@@ -1,10 +1,25 @@
+// Variabili elementi contenitori
+const numbers = document.getElementById("numberContainer");
+
+// Variabili elementi numeri
 const numberOneEl = document.getElementById("numberOne");
 const numberTwoEl = document.getElementById("numberTwo");
 const numberThreeEl = document.getElementById("numberThree");
 const numberFourEl = document.getElementById("numberFour");
 const numberFiveEl = document.getElementById("numberFive");
 
+// Inserisco un numero random da 1 a 100 in ogni elemento
+numberOneEl.innerHTML = getRndNumber(1, 100);
+numberTwoEl.innerHTML = getRndNumber(1, 100);
+numberThreeEl.innerHTML = getRndNumber(1, 100);
+numberFourEl.innerHTML = getRndNumber(1, 100);
+numberFiveEl.innerHTML = getRndNumber(1, 100);
 
-function getRndInteger(min, max) {
+// Generatore numeri random
+function getRndNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+setTimeout( () => {
+    numbers.className = "d-none"
+}, 30000)
