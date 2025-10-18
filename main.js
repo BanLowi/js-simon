4b1ec42a5a87e2c338c0a64daaa59bd0b88d6935
BanLowi <lowi40k@gmail.com>
2025-10-18 12:45:49 +0200

    always different number in fillArray function

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 // Funzione per generare un numero random (inclusi min e max)
 function getRandomdNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 
 // Funzione per riempire un array di numeri random
 function fillArrayRandomNum(array, quantity) {
-    
+
     for (let i = 0; i < quantity; i++) {
-        array.push(getRandomdNum(1, 10));
+        const singleNum = getRandomdNum(1, 10)
+        
+        // SE numero singolo generato NON è nell'array lo pusho ALTRIMENTI ciclo indietro
+        if (!array.includes(singleNum)) {
+            array.push(singleNum);
+        } else {
+            i--
+        }
     }
 }
+
+
 // Array e ciclo per generare 5 numeri random
 let randomNumbers = [];
 fillArrayRandomNum(randomNumbers, 5);
 
 
-// Stampo i numeri in pagina
+// Stampo i numeri in pagina per 30 secondi
 const divEl = document.createElement("div");
 divEl.innerHTML = `
  ${randomNumbers}
 `
 document.body.append(divEl);
+
+setTimeout()
+
